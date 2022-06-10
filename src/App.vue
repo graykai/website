b94c617d30e9d655e22f26b8046b4156f731c6cd
Grayson Hay <grayson@codesee.io>
2022-06-09 19:59:00 -0700

    fills in initial info about Doom Sparkle

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,20 @@
 <script setup>
-import Logo from "./components/assets/Logo.vue"
-import Container from "./components/layout/Container.vue"
-import ContentBox from "./components/ContentBox.vue"
-import Columns from "./components/layout/Columns.vue"
-import SocialIcon from "./components/assets/SocialIcon.vue"
+import Logo from "./components/assets/Logo.vue";
+import Container from "./components/layout/Container.vue";
+import ContentBox from "./components/ContentBox.vue";
+import Columns from "./components/layout/Columns.vue";
+import SocialIcon from "./components/assets/SocialIcon.vue";
 
-const basisClass = {
-}
+const basisClass = {};
 
 const socialUrls = {
-  "mailchimp": "<newsletter>",
-  "github": "https://github.com/graykai",
+  // mailchimp: "<newsletter>",
+  github: "https://github.com/graykai",
   // "discord": "<some link>",
   // "instagram": "https://instagram.com/graykai"
-}
+};
 
-const pastProjects = [
-]
+const pastProjects = [];
 </script>
 
 <template>
@@ -25,34 +23,55 @@ const pastProjects = [
   </div>
   <div class="bg-graykai">
     <Container
-      extraClasses="text-2xl text-gray-100 pt-20 pb-12 text-center font-baseFont font-normal tracking-tight prose-a:underline">
+      extraClasses="text-2xl text-gray-100 pt-20 pb-12 text-center font-baseFont font-normal tracking-tight prose-a:underline"
+    >
       <p class="m-auto">
         graykai studios is a collaboration<br />
         between <a href="">Kai Curtis</a> &amp; <a href="">Grayson Hay</a>
       </p>
     </Container>
   </div>
-  <Container extraClasses="m-36 text-4xl text-center font-baseFont font-bold tracking-wide">
-    <p class="w-1/2 m-auto">we create installations that bridge reality to the fantasy worlds in our heads.</p>
+  <Container
+    extraClasses="m-36 text-4xl text-center font-baseFont font-bold tracking-wide"
+  >
+    <p class="w-1/2 m-auto">
+      we create installations that bridge reality to the fantasy worlds in our
+      heads.
+    </p>
   </Container>
   <Container>
     <Columns extraClasses="m-auto">
-      <ContentBox title="current project" color="red" baseColor="#1C204F" fillColor="#E8E9F3"
-        :extraClasses="basisClass">
+      <ContentBox
+        title="current project"
+        color="red"
+        baseColor="#1C204F"
+        fillColor="#E8E9F3"
+        :extraClasses="basisClass"
+      >
         <div class="flex flex-row gap-5 items-baseline">
           <h1 class="font-headerFont font-bold text-xl">Doom Sparkle</h1>
-          <h2 class="font-headerFont text-sm"><a href="">Lusio 2022</a></h2>
+          <h2 class="font-headerFont text-sm"><!-- TDB --></h2>
         </div>
         <hr />
         <video class="aspect-video" />
         <p class="prose my-3">
-          Something about the project here
+          Just beyond our vision, at the threshold between the real and the
+          imaginary, Doom Sparkle is a gateway that challenges the viewer to
+          stare into what they are, who they are, and see how they are changed
+          by the act of watching.
         </p>
-        <a href="/work/lusio-2022" class="text-right block">Read More…</a>
+        <!-- <a href="/work/lusio-2022" class="text-right block">Read More…</a> -->
       </ContentBox>
-      <ContentBox title="past projects" color="green" baseColor="#1C204F" fillColor="#E8E9F3" :extraClasses="basisClass"
-        v-if="pastProjects.length">
-        Something here</ContentBox>
+      <ContentBox
+        title="past projects"
+        color="green"
+        baseColor="#1C204F"
+        fillColor="#E8E9F3"
+        :extraClasses="basisClass"
+        v-if="pastProjects.length"
+      >
+        Something here</ContentBox
+      >
     </Columns>
   </Container>
   <div class="bg-graykai text-gray-100 mt-36">
